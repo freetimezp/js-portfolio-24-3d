e71aa73c7e5002765315974cb01559f2236e4b84
freetimezp <kraftukrnet@ukr.net>
2023-07-10 15:00:54 +0300

    02. js-portfolio-24-3d. Nav Component.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import './App.css';
 
+import Nav from './components/nav';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Nav />
       <App />
     </BrowserRouter>
   </React.StrictMode>
